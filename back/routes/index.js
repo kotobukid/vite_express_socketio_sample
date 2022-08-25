3520180130cd0b46a1b1dc55919cd5cea507ded0
kotobukid <kotobuki@daqua.jp>
2022-08-25 13:45:54 +0900

    アプリのエントリポイント(index.html)を優先的にレスポンスし、そこから読み込まれるassetsをexpressのstaticミドルウェアに処理させることで、viteでbuildしたファイルをexpressに乗せて提供できるようになった

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
+    fs.readFile('../front/dist/index.html', 'utf-8', (err, result) => {
+        if (err) throw err;
+        res.end(result)
+    })
 });
 
 router.get('/api/v1/users.json', (req, res, next) => {
